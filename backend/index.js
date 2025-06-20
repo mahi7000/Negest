9f25cb73cfc59a7ad0aceb9f625a6c6d93a6ba74
mahi7000 <mahlet7000@gmail.com>
2025-06-20 10:02:10 +0300

    Update database connection

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
-const port = 4000;
+require('dotenv').config();
+
+const port = process.env.PORY || 4000;
 
 const express = require('express');
 const app = express();
@@ -11,9 +13,10 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+const MONGO_URI = process.env.MONGO_URI;
 
 // Database Connection With MongoDb
-mongoose.connect("mongodb+srv://mahlet7000:Iwontforget7@cluster0.8ttqk.mongodb.net/NegestShopping?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
